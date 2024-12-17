923984d39d841ca2503fb071086dcee40f72527f
Dominique Boeckli <dboeckli@gmail.com>
2024-12-17 08:31:41 +0100

    Initial commit

---
+++ src/test/java/ch/dboeckli/springframeworkguru/spring6icecoldmicroservice/Spring6IcecoldMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.dboeckli.springframeworkguru.spring6icecoldmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6IcecoldMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
