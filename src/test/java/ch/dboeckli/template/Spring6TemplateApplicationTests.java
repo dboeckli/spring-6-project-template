1bff9780a826a792c8841fe03dfa11174532caed
adm-boeckli <dboeckli@gmail.com>
2024-12-17 08:50:58 +0100

    add todos for template

--- src/test/java/ch/dboeckli/template/Spring6TemplateApplicationTests.java
+++ src/test/java/ch/dboeckli/template/Spring6TemplateApplicationTests.java
@@ -1,9 +1,11 @@
 package ch.dboeckli.template;
+// TODO: RENAME PACKAGE
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+// TODO: RENAME ME
 class Spring6TemplateApplicationTests {
 
     @Test
