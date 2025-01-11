20bd3c8092cf8bd19a8917466f8d1d5b32b8f8a0
Dominique Boeckli <dboeckli@gmail.com>
2025-01-11 12:04:50 +0100

    improve test (#29)

--- src/test/java/ch/dboeckli/template/Spring6TemplateApplicationTest.java
+++ src/test/java/ch/dboeckli/template/Spring6TemplateApplicationTest.java
@@ -1,15 +1,18 @@
 package ch.dboeckli.template;
 // TODO: RENAME PACKAGE
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Slf4j
 // TODO: RENAME ME
 class Spring6TemplateApplicationTest {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Template Application...");
     }
 
 }
