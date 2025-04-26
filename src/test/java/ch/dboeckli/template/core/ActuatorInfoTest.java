981bf9bdd63389c8643cabb9a8c08d1b99d11b33
dboeckli <dboeckli@gmail.com>
2025-04-26 09:40:54 +0200

    rename profile to local

--- src/test/java/ch/dboeckli/template/core/ActuatorInfoTest.java
+++ src/test/java/ch/dboeckli/template/core/ActuatorInfoTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Slf4j
+@ActiveProfiles("local")
 class ActuatorInfoTest {
 
     @Autowired
