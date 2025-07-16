90fc0833d81cf3e3f71e2dd2b8b75229976d5320
Dominique Boeckli <dboeckli@gmail.com>
2025-07-16 18:00:56 +0200

    fix renovate 7

--- .run/scripts/deploy-k8s.ps1
+++ .run/scripts/deploy-k8s.ps1
@@ -4,4 +4,4 @@ $file = Get-ChildItem -Filter spring-6-project-template-v*.tgz | Select-Object -
 tar -xvf $file.Name
 
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
-helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME --namespace spring-6-project-template --create-namespace --wait --timeout 8m --debug --render-subchart-notes
+helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME -f "./$APPLICATION_NAME/dependencies-values.yaml" --namespace spring-6-project-template --create-namespace --wait --timeout 8m --debug --render-subchart-notes
